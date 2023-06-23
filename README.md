18d62ff9bcd588ea4cd6a4d97ff4a97e9f2be9aa
shreem06 <77281617+shreem06@users.noreply.github.com>
2023-06-23 16:18:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tab-js-exercise
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-y2dqwp)
---
+++ script.js
@@ -0,0 +1,13 @@
+function tabFunction(evt, Name) {
+  var i, tabContent, tabSwitch;
+  tabContent = document.getElementsByClassName('tab_Content');
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = 'none';
+  }
+  tabSwitch = document.getElementsByClassName('tab_Switch');
+  for (i = 0; i < tabSwitch.length; i++) {
+    tabSwitch[i].className = tabSwitch[i].className.replace(' active', '');
+  }
+  document.getElementById(Name).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
